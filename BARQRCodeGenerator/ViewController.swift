d638cb40a55de90f7c284cff3e3b43e591776e9c
pasha4uu <pasha7864u@gmail.com>
2018-10-24 01:14:51 +0530

    first

--- BARQRCodeGenerator/ViewController.swift
+++ BARQRCodeGenerator/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var codeSegment: UISegmentedControl!
+  @IBOutlet weak var imageV: UIImageView!
+  @IBOutlet weak var dataTF: UITextField!
+  var filter :CIFilter!
   override func viewDidLoad() {
     super.viewDidLoad()
+    codeSegment.selectedSegmentIndex = 0
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+  @IBAction func generateTap(_ sender: Any) {
+    
+    if let txt = dataTF.text {
+      
+      let data = txt.data(using: .ascii, allowLossyConversion: false)
+      if codeSegment.selectedSegmentIndex == 0 {
+        
+        filter = CIFilter(name: "CIQRCodeGenerator")
+      }
+      else
+      {
+        filter = CIFilter(name: "CICode128BarcodeGenerator")
 
+      }
+      filter.setValue(data, forKey: "inputMessage")
+      let transform = CGAffineTransform(scaleX: 10, y: 10)
+      imageV.image = UIImage(ciImage: filter.outputImage!.transformed(by: transform))
+      
+    }
+  }
+  
 }
 
